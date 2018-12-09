cc3773a11859eb1feee2452571103bf53150f4b2
Joseph Kilgore <joey.a.kilgore@gmail.com>
2018-12-09 09:54:47 -0500

    More initial commits

---
+++ DiscordTestBot/Modules/Misc.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace DiscordTestBot.Modules
+{
+    public class Misc : ModuleBase<SocketCommandContext>
+    {
+        Random random = new Random();
+
+        [Command("zucc")]
+        public async Task Zucc()
+        {
+            await Context.Channel.SendMessageAsync("Give me your data");
+        }
+
+        [Command("echo")]
+        public async Task Echo([Remainder]string message)
+        {
+            var embed = new EmbedBuilder();
+            embed.WithTitle("Echoed message");
+            embed.WithDescription(message);
+            embed.WithColor(new Color(0, 255, 0));
+
+            await Context.Channel.SendMessageAsync("", false, embed);
+        }
+
+
+        string[] dice = new string[] { "<:d4:521170605484015616>",
+                                        "<:d6:521170605488209923>",
+                                        "<:d8:521170608482942996>",
+                                        "<:d10:521170607531098113>",
+                                        "<:d12:521170607547875339>",
+                                        "<:d20:521170608944316456>" };
+
+        [Command("die")]
+        public async Task DieEmoji()
+        {
+            await Context.Channel.SendMessageAsync(dice[random.Next(0, dice.Length)]);
+        }
+    }
+}
---
+++ DiscordTestBot/Utilities.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace DiscordTestBot
+{
+    class Utilities
+    {
+        private static Dictionary<string, string> alerts;
+
+        static Utilities()
+        {
+            string json = File.ReadAllText("SystemLang/Alerts.json");
+            var data = JsonConvert.DeserializeObject<dynamic>(json);
+            alerts = data.ToObject<Dictionary<string, string>>();
+
+
+        }
+
+        public static string GetAlert(string key)
+        {
+            if (alerts.ContainsKey(key)) return alerts[key];
+            return "";
+        }
+    }
+}
